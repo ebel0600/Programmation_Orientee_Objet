f91560f1006f515b132d303429c4a6ff84a580bd
ebel0600 <benjamin.beramis@gmail.com>
2026-01-28 10:13:51 +0100

    test epargne reussi

--- Banque/src/App.java
+++ Banque/src/App.java
@@ -9,7 +9,7 @@ public class App {
         // double solde;
         // double découvert;
         // double montant;
-        
+
         // System.out.println("Quel est le nom du bénéficiaire :");
         // nom = entree.nextLine();
         // System.out.println("Solde du compte :");
@@ -24,61 +24,64 @@ public class App {
         // System.out.println(unCompte+""+autreCompte);
 
         // if(unCompte.comparer(autreCompte))
-        //     System.out.println(unCompte.getNOMPROPRIO()+" dispose d'un solde plus important que "+autreCompte.getNOMPROPRIO());
+        // System.out.println(unCompte.getNOMPROPRIO()+" dispose d'un solde plus
+        // important que "+autreCompte.getNOMPROPRIO());
         // else
-        //     System.out.println(unCompte.getNOMPROPRIO()+" dispose d'un solde moins important que "+autreCompte.getNOMPROPRIO());
+        // System.out.println(unCompte.getNOMPROPRIO()+" dispose d'un solde moins
+        // important que "+autreCompte.getNOMPROPRIO());
         // System.out.println("--------------------");
 
-        //test créditer
-        // System.out.println("Quelle somme "+unCompte.getNOMPROPRIO()+" souhaite-iel ajouter à son compte ?");
+        // test créditer
+        // System.out.println("Quelle somme "+unCompte.getNOMPROPRIO()+" souhaite-iel
+        // ajouter à son compte ?");
         // montant = entree.nextDouble();
         // entree.nextLine();
         // unCompte.crediter(montant);
         // System.out.println(unCompte+"--------------------");
 
-        //test débiter
-        // System.out.println("Quelle somme "+autreCompte.getNOMPROPRIO()+" souhaite-iel retirer de son compte ?");
+        // test débiter
+        // System.out.println("Quelle somme "+autreCompte.getNOMPROPRIO()+" souhaite-iel
+        // retirer de son compte ?");
         // montant = entree.nextDouble();
         // entree.nextLine();
         // if(autreCompte.debiter(montant))
-        //     System.out.println(autreCompte);
+        // System.out.println(autreCompte);
         // else
-        //     System.out.println("Débit impossible : le découvert autorisé est dépassé !");
+        // System.out.println("Débit impossible : le découvert autorisé est dépassé !");
         // System.out.println("--------------------");
 
-        //test transférer
-        // System.out.println("Combien "+unCompte.getNOMPROPRIO()+" souhaite-iel donner à "+autreCompte.getNOMPROPRIO()+" ?");
+        // test transférer
+        // System.out.println("Combien "+unCompte.getNOMPROPRIO()+" souhaite-iel donner
+        // à "+autreCompte.getNOMPROPRIO()+" ?");
         // montant = entree.nextDouble();
         // entree.nextLine();
         // if(unCompte.transferer(montant, autreCompte))
-        //     System.out.println(unCompte+""+autreCompte);
+        // System.out.println(unCompte+""+autreCompte);
         // else
-        //     System.out.println("Débit impossible : le découvert autorisé est dépassé pour "+unCompte.getNOMPROPRIO()+"  !");    
+        // System.out.println("Débit impossible : le découvert autorisé est dépassé pour
+        // "+unCompte.getNOMPROPRIO()+" !");
         // System.out.println("--------------------");
 
-        //test epargne
-        Compte_Epargne Lucie = new Compte_Epargne("Meyer", 0,1.5 );
-        System.out.println(Lucie);
-        double[] quinzaines = new double [24];
-        // Random rand = new Random();
-        System.out.println("Veuillez renseigner vos montants à la quinzaine de chaque mois");
-        Lucie.crediter(100);
-        for (int i = 0; i<quinzaines.length;i++){
-
-            Lucie.crediter(50);
-            quinzaines[i] = Lucie.getSolde();
-            System.out.println("Quinzaine numero "+(i+1)+" : "+quinzaines[i]);
+        // test epargne
+        Compte_Epargne Epargne = new Compte_Epargne("Meyer", 0, 1.5);
+        double[] quinzaines = new double[24];
+        // System.out.println("Veuillez renseigner vos montants à la quinzaine de chaque mois");
+        Epargne.crediter(22950);
+        for (int i = 0; i < quinzaines.length; i++) {
+            // if(i>=15)
+            // Epargne.setTaux(2.5);
+            // Epargne.crediter(50);
+            quinzaines[i] = Epargne.getSolde();
+            // System.out.println("Quinzaine numero "+(i+1)+" : "+quinzaines[i]+", taux à
+            // "+Epargne.getTaux()+"%");
         }
-        Lucie.calculerInterets(quinzaines);
+        Epargne.calculerInterets(quinzaines);
 
-        System.out.println("Valeur théorique d'intérêt perçus sur le livret A à 100e + 50 supplémentaires par quinzaine consécutivement :\n"+ Lucie);
+        System.out.println(Epargne);
         entree.close();
 
         System.out.println("Programme terminé !");
 
     }
 
-
 }
-
-
